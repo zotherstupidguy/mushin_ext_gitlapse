130a7985b453309035ce5f720b6a9dda15bc7fcd
zotherstupidguy <zotherstupidguy@gmail.com>
2016-12-21 11:41:43 +0200

    adds spec

--- spec/Gitlapse_spec.rb
+++ spec/Gitlapse_spec.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require_relative 'spec_helper'
 
 class GitlapseTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,6 +6,6 @@ class GitlapseTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true 
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'Gitlapse'
+require 'mushin_ext_gitlapse'
 
 require 'minitest/autorun'
